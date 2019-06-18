e0506e490cab50b4d8ec3966e03a6cc295f38f3a
unknown <nemo>
2019-06-18 12:11:32 -0300

    escribo un texto

---
+++ Clase/Clase/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Clase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            try{
+                throw new Exception();
+            }
+            catch(Exception e)
+            {
+                throw;
+            }
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,2 @@
+oh tu amor me envuelve, me sostiene, amor sin condicion, me persigue y deja las 99 y va por mi, no puedo ganarlo, ni merecerlo, tu amor se entrego por mi, tu tu tu tu tu tu tu tu tu tu tu tuuuuu tuuuu
+a ver
